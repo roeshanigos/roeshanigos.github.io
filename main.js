e29986d90aaccae5663ebe24272f80b5c8208830
Roesha Nigos <ishanigos@Roeshas-MacBook-Pro.local>
2018-07-06 23:14:44 -0700

    updated

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ $(document).ready(() => {
 	$(".name").hide(0).delay(500).fadeIn(1000);
 	$(".more-about").hide(0).delay(1200).fadeIn(500);
 
-	$(".moving-nav").hide();
+	$(".topnav").hide();
    $(window).scroll(() =>{  //Listen for the window's scroll event
 		if (isScrolledAfterElement('#about')) { //if it has scrolled beyond the #content elment
-			$('.moving-nav').fadeIn(500);  //Show the navigation bar
+			$('.topnav').fadeIn(500);  //Show the navigation bar
 		} else {
-		  $('.moving-nav').fadeOut(500); //Else hide it
+		  $('.topnav').fadeOut(500); //Else hide it
 		}
   	});
 });
@@ -20,7 +20,25 @@ function isScrolledAfterElement(elem) {
 	var docViewTop = $window.scrollTop();
 	var docViewBottom = docViewTop + $window.height();
 
-	var elemTop = $elem.offset().top + 850;
+	var elemTop = $elem.offset().top + 800;
 	//console.log($elem.offset());
 	return elemTop <= docViewBottom;
+}
+
+
+function myFunction() {
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+				x.className += " responsive";
+		} else {
+				x.className = "topnav";
+		}
+}
+function myFunction() {
+		var x = document.getElementById("staticNav");
+		if (x.className === "static-nav") {
+				x.className += " responsive";
+		} else {
+				x.className = "static-nav";
+		}
 }
