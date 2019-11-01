d300d4919b7044cdc5a3f8269e4ea73d7f6b04f1
Roesha Nigos <ishanigos@Roeshas-MacBook-Pro.local>
2019-11-01 09:05:37 -0500

    update

---
+++ untitled.js
@@ -0,0 +1,42 @@
+test('adds 5 + 2 to equal 7', () => {
+    expect(sum(5, 2)).toBe(7);
+})
+
+test('adds -10 + 2 to equal 7', () => {
+    expect(sum(-10, 2)).toBe(-8);
+})
+
+'adds 2 + 2 to equal 4', () => {
+    expect(sum(2, 2)).toBe(4);}
+
+);
+
+
+
+
+'adds 2 + 2 to equal 4', () => {
+    expect(sum(2, 2)).toBe(4);}
+    
+    'adds 5 + 2 to equal 7', () => {
+    expect(sum(5, 2)).toBe(7);},
+      
+      'adds -10 + 2 to equal -8', () => {
+    expect(sum(-10, 2)).toBe(-8);
+
+
+
+function isprime(num) { 
+	var N = num + 1 ; 
+	var numbers = Array.apply(null, {length: N}).map(Number.call, Number)
+	var factors = [for (i of numbers) if (i % 2 === 0) i];
+	console.log(evens); // logs 2,22,30
+}
+
+module.exports = isprime;
+
+
+var N = 10 + 1 ; 
+Array.apply(null, {length: N}).map(Number.call, Number)
+var numbers = [1, 2, 3, 21, 22, 30];
+var evens = [for (i of numbers) if (i % 2 === 0) i];
+console.log(evens); // logs 2,22,30
